780b6fd5667dfde16c12707d540544ae6115e81f
Rebecca Hass <rebecca.hass91@googlemail.com>
2023-10-18 16:33:30 +0200

    created EntryView, ConfigComp, HeadComp

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  <router-view />
+  <header>
+    <HeaderComponent />
+  </header>
+  <main>
+    <router-view />
+  </main>
 </template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+
+export default {
+  components: { HeaderComponent },
+};
+</script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <header class="header">
+    <section class="header-section">
+      <p>Quiz Game<span>💡</span></p>
+      <nav class="header-navigation"></nav>
+    </section>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header {
+  backdrop-filter: blur(21px) saturate(172%);
+  -webkit-backdrop-filter: blur(21px) saturate(172%);
+  background-color: rgba(163, 175, 245, 0.8);
+  margin-bottom: 20px;
+  padding: 10px 10px;
+}
+.header-section {
+  padding: 10px 10px;
+  font-size: 1.5rem;
+}
+</style>
